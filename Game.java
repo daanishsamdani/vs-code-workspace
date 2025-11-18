d399ae7d07d12df451bf6abfd7753a2f98bf6d85
Daanish Samdani <daanish.samdani@gmail.com>
2025-11-18 22:41:53 +0100

    checkin code

--- Game.java
+++ Game.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Game {
 
     public static void main(String[] args) {
-        Scanner Hand = new Scanner(System.in);
 
     }
 }
--- RoundAndRound.java
+++ RoundAndRound.java
@@ -32,5 +32,29 @@ public class RoundAndRound {
         System.out.println("circle2.area = " + Math.round(circle2.area * 1000) / 1000.0);
 
 
+
+        Cone cone1 = new Cone();
+        Cone cone2 = new Cone();
+        cone1.base = circle1;
+        cone2.base = circle2;
+
+
+        cone1.height = 10.3;
+        cone1.volume = ((1.0 / 3) * cone1.base.area * cone1.height);
+        cone1.surfaceArea = cone1.base.area * (Math.PI * Math.pow(cone1.base.radius, 2) + (cone1.base.radius * Math.PI * Math.sqrt(Math.pow(cone1.height , 2) + Math.pow(cone1.base.radius, 2))));
+
+
+        cone2.height = 10.3;
+        cone2.volume = ((1.0 / 3) * cone2.base.area * cone2.height);
+        cone2.surfaceArea = cone2.base.area * (Math.PI * Math.pow(cone2.base.radius, 2) + (cone2.base.radius * Math.PI * Math.sqrt(Math.pow(cone2.height , 2) + Math.pow(cone2.base.radius, 2))));
+
+
+        System.out.println("cone1.volume = " + Math.round(cone1.volume * 1000) / 1000.0);
+        System.out.println("cone1.surfaceArea = " + cone1.surfaceArea);
+
+        System.out.println("cone2.volume = " + Math.round(cone2.volume * 1000) / 1000.0);
+        System.out.println("cone2.surfaceArea = " + cone2.surfaceArea);
+
+
     }
 }
